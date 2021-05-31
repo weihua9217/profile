ef955cd97429351a79a50c74637647f25dcea326
weihua9217 <weihua9217@gmail.com>
2021-05-31 18:51:15 +0800

    update

--- src/components/bodyComponents/autobiography.js
+++ src/components/bodyComponents/autobiography.js
@@ -2,7 +2,7 @@ const Autobiography = () => {
     return(    
         <section class="resume-section" id="autobiography">
         <div class="resume-section-content">
-            <h2 class="mb-5">Autobiography</h2>
+            <h2 class="mb-5">自傳 Autobiography</h2>
             <ul class="fa-ul mb-0">
                 我是李偉華，生長於桃園南崁，高中畢業於中央大學附屬中壢高中，目前就讀政治大學資訊科學系四年級。
                 從小就對新的事物抱有強烈的好奇心。高中時對數學有較大的興趣，喜歡定義問題，再找尋問題的解決方法。
