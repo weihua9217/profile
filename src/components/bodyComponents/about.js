50b94c5c61dd92a513c862e31f6dfd452fb01fa9
Matthew Lee <weihua9217@gmail.com>
2021-09-28 22:27:39 +0800

    Update about.js

--- src/components/bodyComponents/about.js
+++ src/components/bodyComponents/about.js
@@ -16,7 +16,7 @@ const About = () => {
                         contact me &nbsp;: &nbsp;
                         <a class="text-primary">weihua9217[at]gmail.com</a>
                     </div>
-                    <p class="lead mb-5">目前就讀國立政治大學資訊科學系四年級，平時樂於參加演講及社群，了解最新的技術。對新的人事物懷有好奇心，並保持著謙卑的態度努力的學習，期許自己能靠資訊的力量為社會盡一份心力。</p>
+                    <p class="lead mb-5">目前就讀國立政治大學資訊科學系碩士一年級，平時樂於參加演講及社群，了解最新的技術。對新的人事物懷有好奇心，並保持著謙卑的態度努力的學習，期許自己能靠資訊的力量為社會盡一份心力。</p>
                     <div class="social-icons">
 //                         <a class="social-icon" href="https://github.com/weihua9217"><i class="fab fa-github"></i></a>
 //                         <a class="social-icon" href="https://www.facebook.com/weihua9217/"><i class="fab fa-facebook-f"></i></a>
